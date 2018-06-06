828b8dfd2e6320b5f5c51bf021a23f2edebd4fa3
ftorto <dev.torto@gmail.com>
2018-06-06 08:43:32 +0200

    [#173031] comments added
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- assets/entry_point.sh
+++ assets/entry_point.sh
@@ -4,5 +4,11 @@ python3 ./main.py
 
 export PATH=$PATH:node_modules/.bin
 
+# Clean former build path
 gulp clean
+
+# Copy sources to build path
+gulp prepare
+
+# Modify sources to integrate viztools
 gulp build
